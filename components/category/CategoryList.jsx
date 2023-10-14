7215a9c33d76ea31a95b0a13282cdd3bdd7ac6b4
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-10-14 20:44:21 +0600

    Update CategoryList.jsx

--- components/category/CategoryList.jsx
+++ components/category/CategoryList.jsx
@@ -1,45 +1,71 @@
 import React, { useState } from "react";
-import IconArrowDown from "@/public/Nordcom-Icons/IconArrowDown";
-import IconArrowUp from "@/public/Nordcom-Icons/IconArrowUp";
+import IconArrowLeft from "@/public/Nordcom-Icons/IconArrowLeft";
 
-const CategoryList = ({ categories }) => {
-  const [openCategory, setOpenCategory] = useState(null);
+const CategoryList = () => {
+  const categoriesData = {
+    Apple: ["iPhone", "Charger", "Case", "Screen Protector", "Accessories"],
+    iPhone: [
+      "iPhone 15",
+      "iPhone 15 Plus",
+      "iPhone 15 Pro",
+      "iPhone 15 Pro Max",
+      "iPhone 14",
+      "iPhone 14 Pro",
+      "iPhone 14 Pro Max",
+      "iPhone 14 Plus",
+      "iPhone 13",
+      "iPhone SE",
+      "Used",
+    ],
+    // Add more categories as needed
+  };
 
-  const toggleCategory = (index) => {
-    if (openCategory === index) {
-      setOpenCategory(null);
+  const [categories, setCategories] = useState(categoriesData);
+  const [currentCategory, setCurrentCategory] = useState("Apple");
+  const [prevCategories, setPrevCategories] = useState([]);
+
+  const handleCategoryClick = (category) => {
+    setPrevCategories([...prevCategories, currentCategory]);
+    setCurrentCategory(category);
+  };
+
+  const handleGoBack = () => {
+    if (prevCategories.length > 0) {
+      const previousCategory = prevCategories.pop();
+      setCurrentCategory(previousCategory);
+      setPrevCategories([...prevCategories]);
     } else {
-      setOpenCategory(index);
+      // If no previous category, set to "Home"
+      setCurrentCategory("Home");
     }
   };
 
   return (
-    <ul>
-      {categories.map((category, index) => (
-        <li key={index}>
-          <div
-            className={`${
-              openCategory === index
-                ? "border border-primary-19 font-medium"
-                : "bg-white font-normal"
-            } p-2 rounded flex items-center justify-between cursor-pointer w-68 h-10 px-4 gap-44 flex-shrink-0`}
-            onClick={() => toggleCategory(index)}
+    <div>
+      <div className="flex items-center cursor-pointer" onClick={handleGoBack}>
+        <IconArrowLeft className="mr-2" />
+        <span className="text-sm">
+          {prevCategories.length > 0
+            ? prevCategories[prevCategories.length - 1]
+            : "Home"}
+        </span>
+      </div>
+      <h4 className="font-bold my-2 ml-2">{currentCategory}</h4>
+      <hr className="mb-2 ml-2 bg-black h-0.5" />
+      <ul className="font-semibold ml-2">
+        {categories[currentCategory]?.map((subcategory) => (
+          <li
+            key={subcategory}
+            className={`cursor-pointer text-sm font-medium bg-primary-31 mb-1 px-2 py-2 rounded-[5px] ${
+              currentCategory === subcategory ? "bg-black text-white" : ""
+            }`}
+            onClick={() => handleCategoryClick(subcategory)}
           >
-            {category.name}
-            {category.subcategories.length > 0 && (
-              <span className="ml-2 cursor-pointer">
-                {openCategory === index ? <IconArrowUp /> : <IconArrowDown />}
-              </span>
-            )}
-          </div>
-          {openCategory === index && (
-            <ul className="pl-4 cursor-pointer">
-              <CategoryList categories={category.subcategories} />
-            </ul>
-          )}
-        </li>
-      ))}
-    </ul>
+            {subcategory}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
